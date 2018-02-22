31401385d797832b6404ed18cebf807df325aea1
Jennifer M Fung <j1fung@ieng6-202.ucsd.edu>
2018-02-22 14:24:45 -0800

    fixed lcs, still has cout

--- LCS.cpp
+++ LCS.cpp
@@ -13,16 +13,16 @@
 
 std::string s = "";
 std::string backtrack( TwoD_Array<int> * T, std::string s1, std::string s2, int i, int j){
-	if(i == 0 || j == 0){
+	if(i == -1 || j == -1){
 		return s;
 	}
 	if(s1[i] == s2[j]){
-	 	s.push_back(s1[i]);	
-	 	
+	// 	s.push_back(s1[i]);	
+	 s = s1[i] + s;	
 		std::cout<<"s: " << s << std::endl;
 		return backtrack(T, s1, s2, i-1, j-1) + s;
 	}
-	if( T->at(i, j-1) > T->at(i-1, j)){
+	if( T->at(i+1, j) > T->at(i, j+1)){
 		return backtrack(T, s1, s2, i, j-1);
 	}
 	return backtrack(T, s1, s2, i-1, j);
@@ -34,24 +34,25 @@ std::string lcs(std::string s1, std::string s2) {
 	int n = s2.length();
 	std::cout<<"s1 len: " << n << std::endl;
 	std::cout<<"s2 len: " << m << std::endl;
-	TwoD_Array<int> * T = new TwoD_Array<int>(m,n);
+	TwoD_Array<int> * T = new TwoD_Array<int>(m+1,n+1);
 	//initialize left col
-	for(int i = 0; i < m; i++){
+	for(int i = 0; i <= m; i++){
 		T->at(i,0) = 0;
 	}
 	//initialize top row
-	for(int j = 0; j < n; j++){
+	for(int j = 0; j <= n; j++){
 		T->at(0,j) = 0;
 	}
 	//iterate thru arr to get the length of the LCS
-	for(int i = 1; i < m; i++){
-		for( int j = 1; j < n; j++){
-		//std::cout<< "s1[i]= "<<s1[i]<<" s2[j]= "<<s2[j] << std::endl;
+	for(int i = 0; i < m; i++){
+		for( int j = 0; j < n; j++){
+	
 			if(s1[i] == s2[j]){
-				T->at(i,j) = T->at(i-1, j-1) + 1;
+			std::cout<< "s1[i]= "<<s1[i]<<" s2[j]= "<<s2[j] << std::endl;	
+				T->at(i+1,j+1) = T->at(i, j) + 1;
 			}
 			else{
-				T->at(i,j) = MAX(T->at(i,j-1), T->at(i-1,j));
+				T->at(i+1,j+1) = MAX(T->at(i+1,j), T->at(i,j+1));
 			}
 		}
 	}
